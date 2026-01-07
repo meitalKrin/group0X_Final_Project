21be911b2ec1ce062ac35bc1a569b3590a9c7b12
Bent40 <sjgrbent40@gmail.com>
2026-01-07 12:54:52 +0200

    Server now successfully sends messages between 2 connections via msg. Also added explanations to the files.

--- part_two/client.py
+++ part_two/client.py
@@ -1,40 +1,39 @@
 import socket
 import threading
 
-HOST = "127.0.0.1"
-PORT = 5000
+HOST = "127.0.0.1" # Localhost
+PORT = 5000 # Server Port
 
 def receive_messages(sock):
-    while True:
+    while True: # Message Receiving loop
         try:
-            data = sock.recv(4096)
+            data = sock.recv(4096) #breaking down the data
             if not data:
                 break
-            print("\n" + data.decode("utf-8").strip())
+            print("\n" + data.decode("utf-8").strip()) # Printing it onto console
         except:
             break
 
-def main():
-    username = input("Choose username: ")
+async def main ():
+    username = input("Choose username: ") 
 
-    with socket.create_connection((HOST, PORT)) as sock:
-        sock.sendall(f"HELLO {username}\n".encode("utf-8"))
+    with socket.create_connection((HOST, PORT)) as sock: #creating a new connection
+        threading.Thread(target=receive_messages, args=(sock,), daemon=True).start() # Setting the thread for receiving messages
 
-        threading.Thread(target=receive_messages, args=(sock,), daemon=True).start()
+        await sock.sendall(f"Hello {username}\n".encode("utf-8")) # sending register command
 
         print("Commands:")
-        print("  CHAT <username>")
         print("  MSG <message>")
         print("  QUIT")
 
-        while True:
-            msg = input("> ")
-            if not msg:
+        while True: # Main input loop
+            msg = input("> ") #receiving commands
+            if not msg: # If message is empty, just move on
                 continue
 
-            sock.sendall((msg + "\n").encode("utf-8"))
+            sock.sendall((msg + "\n").encode("utf-8")) # Sending the command received to the server
 
-            if msg.upper() == "QUIT":
+            if msg.upper() == "QUIT": # breaking the loop and killing the client
                 break
 
 if __name__ == "__main__":
--- part_two/server.py
+++ part_two/server.py
@@ -4,44 +4,64 @@ import threading
 HOST =  "0.0.0.0"
 PORT = 5000
 
-clients = set()
+clients = {}
 clients_lock = threading.Lock()
 
 def handle_client(conn: socket.socket, addr):
     print(f"[+] Connected: {addr}")
+    username = ""
     with conn:
         try:
             while True:
                 data = conn.recv(4096)  # blocking read
                 if not data:
                     break  # client disconnected
-
-                msg = data.decode("utf-8", errors="replace").strip()
+        
+                msg = data.decode("utf-8", errors="replace").strip() # Decoding the data
+                command = msg.split(" ", 1) # Getting the specific command used
                 print(f"[{addr}] {msg}")
 
-                # Echo back (for testing)
-                conn.sendall(f"Server got: {msg}\n".encode("utf-8"))
-        except ConnectionError:
+                if(command[0].lower() == "hello"): # Registering to the client list and keeping username
+        
+                    with clients_lock: # Locking client threads to avoid mismatch and errors mid register.
+                        clients[command[1]] = conn #Saving connection under username
+                        username = command[1] # Saving username for future message use
+                
+                elif(command[0].lower() == "msg"): #Sending a message to a specific user
+                
+                    toSend = command[1].split(" ", 1) # Extracting username and message to pass
+                    receiver, msg_to_send = clients.get(toSend[0]), f"{username}: {toSend[1]}".encode("utf-8") # Getting conn via username and reformatting message to also hold sender name
+                
+                    try:
+                        if(receiver):
+                            receiver.send(msg_to_send)
+                
+                        else: # If no such user exists
+                            conn.send(b"User Not Found")
+                
+                    except Exception as e: # for any unexpected errors
+                        print(e)
+                        conn.send(b"There was an error while attempting to send message")
+                else:
+                    conn.send(b"Unrecognized Command")
+        except ConnectionError: # For sudden connection issues and disconnects
             pass
-        finally:
-            with clients_lock:
-                clients.discard(conn)
             print(f"[-] Disconnected: {addr}")
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((HOST, PORT))
-        server.listen(10)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: # setting the server up
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Options for hte socket
+        server.bind((HOST, PORT)) # Binding To 0.0.0.0 and port of our choice
+        server.listen(10) 
+        
         print(f"Server listening on {HOST}:{PORT}")
         
-        while True:
-            conn, addr = server.accept()
-            with clients_lock:
-                clients.add(conn)
-                print(f"Connected by {addr}")
-            t = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
-            t.start()
+        while True: # Main server loop
+            conn, addr = server.accept() # Receiving clients 
+            print(f"Connected by {addr}") # Announcing Connection
+        
+            t = threading.Thread(target=handle_client, args=(conn, addr), daemon=True) # Creating a new listening thread for the connected client
+            t.start() # starting the thread
             
 if __name__ == "__main__":
     main()
